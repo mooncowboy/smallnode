44d37baef0e73cd1d09b186749f754cd8aef666b
Ricardo Fiel <ricardo.fiel@rupeal.com>
2016-09-29 12:11:35 +0100

    Testing continous image deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var server = http.createServer(function(req, res) {
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.write('{ "status": "ok from semaphore" }');
+  res.write('{ "status": "ok" }');
   res.end();
 });
 server.listen(8080);
